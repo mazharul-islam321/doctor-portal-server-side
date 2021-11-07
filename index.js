ee0428b7088564d4dda8f569c299d344dd2003e5
mazharul-islam321 <https://github.com/mazharul-islam321/School-Management-System.git>
2021-11-08 03:12:11 +0600

    first add 50% of code

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const app = express();
+require("dotenv").config();
+var cors = require("cors");
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lw2vc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+// console.log(uri);
+
+async function run() {
+    try {
+        // Connect the client to the server
+        await client.connect();
+        const database = client.db("doctors_portal");
+        const appointmentsCollection = database.collection("appointments");
+
+        // get api
+        app.get("/appointments", async (req, res) => {
+            const email = req.query.email;
+            const date = new Date(req.query.date).toLocaleDateString();
+            const query = { email: email, date: date };
+            const cursor = appointmentsCollection.find(query);
+            const appointments = await cursor.toArray();
+            res.json(appointments);
+        });
+
+        // post api
+        app.post("/appointments", async (req, res) => {
+            const appointment = req.body;
+            const result = await appointmentsCollection.insertOne(appointment);
+            // console.log(result);
+            res.json(result);
+        });
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+    res.send("Doctor portal api");
+});
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
